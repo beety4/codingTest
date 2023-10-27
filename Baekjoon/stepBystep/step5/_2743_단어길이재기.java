03ea9283398ce584d2579cb7eaabd7b71cf75ad0
beety4 <beety4@naver.com>
2023-10-27 22:56:03 +0900

    step5_start

--- Baekjoon/stepBystep/step5/_2743_단어길이재기.java
+++ Baekjoon/stepBystep/step5/_2743_단어길이재기.java
@@ -5,14 +5,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 // 23.10.27
-// https://www.acmicpc.net/problem/27866
-public class _27866_문자와문자열 {
+// https://www.acmicpc.net/problem/2743
+public class _2743_단어길이재기 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String s = br.readLine();
-		int i = Integer.parseInt(br.readLine());
 		
-		System.out.println(s.charAt(i-1));
-
+		System.out.println(s.length());
 	}
 }
