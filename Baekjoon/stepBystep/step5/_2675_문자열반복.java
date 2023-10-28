74867c940373958ea1d2a3bb7bdf3778dcccfbce
beety4 <beety4@naver.com>
2023-10-28 15:50:10 +0900

    step5_ing-3

---
+++ Baekjoon/stepBystep/step5/_2675_문자열반복.java
@@ -0,0 +1,29 @@
+package Baekjoon.stepBystep.step5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 23.10.28
+// https://www.acmicpc.net/problem/2675
+public class _2675_문자열반복 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<t; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int plus = Integer.parseInt(st.nextToken());
+			String s = st.nextToken();
+			
+			for(int j=0; j<s.length(); j++) {
+				for(int k=0; k<plus; k++) {
+					System.out.print(s.charAt(j));
+				}
+			}
+			System.out.println();
+		}
+
+	}
+}
