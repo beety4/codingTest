d3d32c39eb2f2d904bcb3b41950eaaed50dd7740
beety4 <beety4@naver.com>
2023-11-12 14:52:48 +0900

    a

---
+++ Baekjoon/stepBystep/step08/_2292_벌집.java
@@ -0,0 +1,16 @@
+package Baekjoon.stepBystep.step08;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 23.11.07
+// https://www.acmicpc.net/problem/2292
+public class _2292_벌집 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+
+	}
+}
+ 
