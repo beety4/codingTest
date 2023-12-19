b9693531522c0ab0002f10f2d88fc2d5f9aca1fe
beety4 <beety4@naver.com>
2023-12-19 23:52:40 +0900

    1979f

---
+++ Baekjoon/stepBystep/step09/_1979_소수찾기.java
@@ -0,0 +1,28 @@
+package Baekjoon.stepBystep.step09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 23.12.19
+// https://www.acmicpc.net/problem/1978
+public class _1979_소수찾기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int repeat = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i=0; i<repeat; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			
+			for(int j=0; j<n; j++) {
+				
+			}
+			
+			
+			
+		}
+		
+	}
+}
