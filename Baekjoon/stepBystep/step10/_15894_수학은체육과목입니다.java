32d611577f809204229278b1acfb8773b11363f2
beety4 <beety4@naver.com>
2023-12-24 00:39:30 +0900

    15894

---
+++ Baekjoon/stepBystep/step10/_15894_수학은체육과목입니다.java
@@ -0,0 +1,17 @@
+package Baekjoon.stepBystep.step10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 23.12.24
+// https://www.acmicpc.net/problem/15894
+public class _15894_수학은체육과목입니다 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(br.readLine());
+		
+		System.out.println(4 * n);
+	}
+
+}
