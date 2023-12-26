a671bae7b829e6e59a1db573fc6ff4ed515c59aa
beety4 <beety4@naver.com>
2023-12-26 22:41:59 +0900

    24262

--- Baekjoon/stepBystep/step11/_24262_알고리즘수행시간_1.java
+++ Baekjoon/stepBystep/step11/_24262_알고리즘수행시간_1.java
@@ -1,15 +1,10 @@
 package Baekjoon.stepBystep.step11;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-// 23.12.25
+// 23.12.26
 // https://www.acmicpc.net/problem/24262
 public class _24262_알고리즘수행시간_1 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(0);
 	}
 }
