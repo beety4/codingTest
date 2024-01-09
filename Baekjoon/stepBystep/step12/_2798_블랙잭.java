8848d7f2beb02c371f1909a5e3267d5a252bb122
beety4 <beety4@naver.com>
2024-01-09 16:57:45 +0900

    2798

--- Baekjoon/stepBystep/step12/_2798_블랙잭.java
+++ Baekjoon/stepBystep/step12/_2798_블랙잭.java
@@ -3,12 +3,9 @@ package Baekjoon.stepBystep.step12;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
-// 23.12.27
+// 24.01.09
 // https://www.acmicpc.net/problem/2798
 public class _2798_블랙잭 {
 	public static void main(String[] args) throws IOException {
@@ -24,47 +21,18 @@ public class _2798_블랙잭 {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		ArrayList<Integer> list = new ArrayList<>();
-		while(true) {
-			if(sum(list) < m) {
-				list.add(arr[0]);
-			}
-			list.add(arr[0]);
-			
-			if(true) {
-				break;
+		int max = 0;
+		for(int j=0; j<n; j++) {
+			for(int k=0; k<j; k++) {
+				for(int l=0; l<k; l++) {
+					int sum = arr[j] + arr[k] + arr[l];
+					if(sum <= m && sum > max) {
+						max = sum;
+					}
+				}
 			}
 		}
 		
-		
-		
-		
-		
-		Arrays.sort(arr, Collections.reverseOrder());
-		int j = 3;
-		int k = 0;
-		int result = 0;
-		while(j != 0) {
-			if(result + arr[k] <= m) {
-				result += arr[k];
-				j--;
-				System.out.println(arr[k] + " // " + j);
-			}
-			
-		}
-		
-		System.out.println(result);
-	}
-
-	public static int sum(ArrayList list) {
-		if(list.isEmpty() == false) {
-			int value = 0;
-			for(int i=0; i<list.size(); i++) {
-				value += (int)list.get(i);
-			}
-			return value;
-		} else {
-			return 0;
-		}
+		System.out.println(max);
 	}
 }
