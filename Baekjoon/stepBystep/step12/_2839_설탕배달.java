085daea80f460104d58538ad2e5c946c8469c10f
beety4 <beety4@naver.com>
2024-02-11 17:38:57 +0900

    2839

--- Baekjoon/stepBystep/step12/_2839_설탕배달.java
+++ Baekjoon/stepBystep/step12/_2839_설탕배달.java
@@ -4,32 +4,25 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 24.02.10
+// 24.02.11
 // https://www.acmicpc.net/problem/2839
 public class _2839_설탕배달 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
-		int a =  n / 5;
-		int b = n % 5;
 		
-		int other = 0;
-		if(b != 0) {
-			for(int i=a; i>=-1; i--) {
-				System.out.println(i);
-				int c = n - 5*(i+1);
-				
-				if(c%3 == 0) {
-					a = i + 1;
-					other = c / 3;
-					break;
-				}
+		int test = n / 3;
+		int min = 9999;
+		for(int i=0; i<=test; i++) {
+			int ab = n - 3*i;
+			
+			if(ab%5 == 0) {
+				int cnt = ab/5 + i;
+				min = cnt < min ? cnt : min;
 			}
 		}
-
-		System.out.println("a : " + a + " // other : " + other);
-		System.out.println(a+other == 0 ? -1 : a + other);
-
+		
+		System.out.println(min == 9999 ? -1 : min);
 	}
 }
